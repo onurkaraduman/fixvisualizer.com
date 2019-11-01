dfb293c53e37db411421cbc634e33a07de7ecdc9
onurkaraduman <onrkrdmn@gmail.com>
2019-11-01 17:44:23 +0100

    initial commit

---
+++ src/app/components/fix-message-accordion/fix-message-accordion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fix-message-accordion',
+  templateUrl: './fix-message-accordion.component.html',
+  styleUrls: ['./fix-message-accordion.component.sass']
+})
+export class FixMessageAccordionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
