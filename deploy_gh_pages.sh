a08c82d275d3ae93d902666890676eef0502e162
onurkaraduman <bb18temmuz>
2019-11-09 23:31:08 +0100

    deploy script update

--- deploy_gh_pages.sh
+++ deploy_gh_pages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+npm install
 ng build --prod --base-href https://onurkaraduman.github.io/fixed-angular/
 git checkout gh-pages
+git pull
 ls | grep -v dist | xargs rm -r
-cp dist/fixed-angular/* .
+cp -r dist/fixed-angular/* .
 git add .
 git commit -m "update"
 git push origin gh-pages
