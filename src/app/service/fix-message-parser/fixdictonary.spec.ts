ff20b8a6c41b7b67873738db2e1273580a22614d
onurkaraduman <onrkrdmn@gmail.com>
2023-10-22 00:48:23 +0200

    update

--- src/app/service/fix-message-parser/fixdictonary.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Fixdictonary44 } from './fixdictonary';
-
-describe('Fixdictonary44', () => {
-  it('should create an instance', () => {
-    expect(new Fixdictonary44()).toBeTruthy();
-  });
-});
