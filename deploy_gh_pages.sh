f6d776b25f4525664abf45585c3859bcae81af57
onurkaraduman <onrkrdmn@gmail.com>
2023-10-22 00:44:43 +0200

    name update

--- deploy_gh_pages.sh
+++ deploy_gh_pages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 npm install
-ng build --prod --base-href https://onurkaraduman.github.io/fixed-angular/
+ng build --prod --base-href https://onurkaraduman.github.io/fixvisualizer.com/
 git checkout gh-pages
 git pull
 ls | grep -v dist | xargs rm -r
-cp -r dist/fixed-angular/* .
+cp -r dist/fixvisualizer.com/* .
 git add .
 git commit -m "update"
 git push origin gh-pages
